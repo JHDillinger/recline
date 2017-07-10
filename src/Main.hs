6c226d99fa708c2a7edddbd161f581345465f488
JHDillinger <armin.bernstetter@gmail.com>
2017-07-11 00:49:12 +0200

    tried the first example from https://github.com/intolerable/reddit (successfully) - now shows the front page posts

--- src/Main.hs
+++ src/Main.hs
@@ -2,24 +2,48 @@
 
 module Main where
 
---main :: IO ()
---main = do
---  putStrLn "hello world"
+-- import Reddit
+-- import Reddit.Types.User
+--
+-- import Data.List
+-- import Data.Ord
+--
+-- -- instance Show a => Show (APIError a)
+--
+-- custom = RedditOptions True Nothing Anonymous (Just "recline v0.1.0.0")
+--
+-- usersToCheck = ["GallowBoob", "spez"]
+--
+-- main = do
+--     putStrLn "Find the user with the highest karma!"
+--     putStrLn "enter first user"
+--     a <- readLn
+--     putStrLn "enter second user"
+--     b <- readLn
+--     let users = [a,b]
+--     runRedditWith custom $ do
+--             infos <- mapM (getUserInfo . Username) users
+--             return $ maximumBy (comparing linkKarma) infos
 
+import           Reddit
+import           Reddit.Types.Post
 
-import Reddit
-import Reddit.Types.User
-
-import Data.List
-import Data.Ord
-
--- instance Default RedditOptions where
---     def =  RedditOptions True Nothing Anonymous (Just "recline v0.1.0.0")
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Monoid
+import qualified Data.Text              as Text
+import qualified Data.Text.IO           as Text
 
 custom = RedditOptions True Nothing Anonymous (Just "recline v0.1.0.0")
 
-usersToCheck = ["GallowBoob", "spez"]
-
 main = runRedditWith custom $ do
-  infos <- mapM (getUserInfo . Username) usersToCheck
-  return $ maximumBy (comparing linkKarma) infos
+  Listing _ _ posts <- getPosts
+  forM_ posts $ \post -> do
+    liftIO $ Text.putStrLn $
+       "\n[" <> tshow (score post) <> "] " <>
+       title post <> " (" <> sshow (subreddit post) <> ")"
+
+tshow = Text.pack . show
+
+sshow :: SubredditName -> Text.Text
+sshow = Text.pack . show
