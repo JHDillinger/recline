d7722a5e06ddf18b8e4e0aa2debaf4f9740c1a74
JHDillinger <armin.bernstetter@gmail.com>
2017-07-10 20:07:30 +0200

    set up project using https://hackage.haskell.org/package/reddit. Main.hs contains only the second example (highest karma of list of users) for now

---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+--main :: IO ()
+--main = do
+--  putStrLn "hello world"
+
+
+import Reddit
+import Reddit.Types.User
+
+import Data.List
+import Data.Ord
+
+-- instance Default RedditOptions where
+--     def =  RedditOptions True Nothing Anonymous (Just "recline v0.1.0.0")
+
+custom = RedditOptions True Nothing Anonymous (Just "recline v0.1.0.0")
+
+usersToCheck = ["endzeit_", "LMFAOwHOcAREASHAHXd"]
+
+main = runRedditWith custom $ do
+  infos <- mapM (getUserInfo . Username) usersToCheck
+  return $ maximumBy (comparing linkKarma) infos
